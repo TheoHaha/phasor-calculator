181b869325764f186c4517a04c54330f4dfbab6d
Theo <47364817+TheoHaha@users.noreply.github.com>
2019-11-13 00:34:11 +0200

    Update myMain.java

--- src/main_pack/myMain.java
+++ src/main_pack/myMain.java
@@ -3,9 +3,9 @@ package main_pack;
 public class myMain
 {	
 	// 	TODO: Add a converter tab that converts from complex to phasor and vice versa		(coming in v1.x)
-	//	TODO: Make it so you can save a list in a file and load it and stuff				(coming in v1.x)
-	//	TODO: Refine the GUI if it looks weird												(coming in v1.x)
-	//	TODO: Greek localization															(coming in v1.x)
+	//	TODO: Make it so you can save a list in a file and load it and stuff			(coming in v1.x)
+	//	TODO: Refine the GUI if it looks weird							(coming in v1.x)
+	//	TODO: Greek localization								(coming in v1.x)
 	public static void main(String[] args) 
 	{
 		Gui gui = new Gui();
