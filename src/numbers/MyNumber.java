116b15ac6a8216fdc33ced1d68c304e59436acdf
Theo <47364817+TheoHaha@users.noreply.github.com>
2019-11-20 09:50:41 +0200

    Code optimization
    This is no longer an empty class, added a few methods to optimize and tidy up the code

--- src/numbers/MyNumber.java
+++ src/numbers/MyNumber.java
@@ -2,9 +2,29 @@ package numbers;
 
 public class MyNumber
 {
-	/*
-	 *	Empty class!
-	 *
-	 *	Its only purpose is to unite Complex and Phasor under one superclass.
-	 */
+	public static MyNumber parseMyNumber(String s)
+	{
+		if (s.contains("|"))
+			return Phasor.parsePhasor(s);
+		else
+			return Complex.parseComplex(s);
+	}
+	
+	public void show()
+	{
+		if (this.getClass()==new Phasor().getClass())
+			((Phasor)this).show();
+		else if (this.getClass()==new Complex().getClass())
+			((Complex)this).show();
+	}
+	
+	public String toString()
+	{
+		if (this.getClass()==new Phasor().getClass())
+			return ((Phasor)this).toString();
+		else if (this.getClass()==new Complex().getClass())
+			return ((Complex)this).toString();
+		
+		return null;
+	}
 }
